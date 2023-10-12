c180fb142586842e7b86fed5444f23d5a57a9bd1
GashemaKalisa <147123309+GashemaKalisa@users.noreply.github.com>
2023-10-11 23:55:31 -0400

    This is my “Basic_HTML_and_JavaScript_1” folder

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,8 @@
+function My_First_Function() {
+		// body...
+		var String ="Gashema_Kalisa";
+		var result = String.fontcolor("green");
+		document.getElementById("Irish") .innerHTML = 
+		result;
+	}
+	alert("Hello, World")
